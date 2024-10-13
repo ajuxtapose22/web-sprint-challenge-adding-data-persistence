a18b570eac15c271ba55f194aad6bdb0a7dbfe62
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-10-13 14:38:31 -0700

    test wip

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const express = require('express')
-const app = express()
+const server = require('./api/server.js')
 
-const resourceRouter = require('./api/resource/router')
-const projectRouter = require('./api/project/router')
-const taskRouter = require('./api/task/router')
+const port = 5000
 
-app.use(express.json())
-
-
-app.use('/api/resources', resourceRouter)
-app.use('/api/projects', projectRouter)
-app.use('/api/tasks', taskRouter)
-
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
